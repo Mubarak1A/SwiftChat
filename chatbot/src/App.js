08e84cc3cebdd89286184764457369367c797e7d
Mubarak1A <adesinamubarak123@example.com>
2023-10-10 20:36:18 +0100

    restructure files and directories

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 //import ReactDOM from 'react-dom/client';
 import Navbar from './components/navbar/navbar';
-import LandingPage from './components/navbar/landing_page/landing_page';
-import Input from './components/navbar/input/input';
-import Output from './components/navbar/output/output';
+import LandingPage from './components/landing_page/landing_page';
+import Input from './components/input/input';
+import Output from './components/output/output'
 import './App.css';
 
 const App = () => {
