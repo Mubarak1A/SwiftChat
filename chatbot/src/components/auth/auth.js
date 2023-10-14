eb4875baf610c0be8eadf5fa114760f6fab1a6a5
Mubarak1A <adesinamubarak123@example.com>
2023-10-14 19:12:29 +0100

    start hanko auth integration

--- chatbot/src/components/auth/auth.js
+++ chatbot/src/components/auth/auth.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './auth.css'
+import { register, Hanko } from "@teamhanko/hanko-elements";
+
+const HankoApi = process.env.HANKO_API
 
 const Auth = () => {
+
+    useEffect(() => {
+        register({});
+    }, []);
+
     return(
         <div className='auth'>
-
+            <Hanko-auth api={HankoApi} />
         </div>
     );
 }
