25dab7127c5d6106a0c996e54e41cb7766736fbd
Mubarak1A <adesinamubarak123@example.com>
2023-10-22 09:20:06 +0100

    prepare all endpoint with an object as database

--- server/server.js
+++ server/server.js
@@ -1,21 +1,95 @@
 const express = required('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            chats: '',
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            chat: '',
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            email: 'sally@gmail.com',
+            hash: '',
+        }
+    ]
+}
+
 app.get('/', (req, res) => {
-    console.log("It's Working");
+    res.send("It's Working");
 })
 
-app.post('/auth', (req, res) => {
-    console.log('signin');
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('signin successful');
+    } else {
+        res.status(400).json('error loging in')
+    }
 })
 
-app.get('/user_page', (req, res) => {
-    console.log('userPage');
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    database.push({
+        id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+    })
+
+    res.json(database.users[database.users.length-1]);
 })
 
-app.get('/profile', (req, res) => {
-    console.log('profile');
+
+app.put('/chatbot/:id', (req, res) => {
+    const { id } = params;
+    let found = false;
+
+    database.users.forEach((user) => {
+        if (user.id === id){
+            found = true
+            user.chat = req.body
+            return res.json(user);
+        }
+    })
+
+    if (!found){
+        res.status(400).json('user not found')
+    }
+})
+
+app.get('/:id/profile', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach((user) => {
+        if (user.id === id){
+            found = true
+            return res.json(user);
+        }
+    })
+
+    if (!found){
+        res.status(400).json('user not found')
+    }
 })
 
 app.listen(5000);
