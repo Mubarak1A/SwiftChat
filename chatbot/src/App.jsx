838cf5607a571c72ef1be24fd90ceda9cba3f2f1
Mubarak1A <adesinamubarak123@example.com>
2023-10-28 09:25:30 +0100

    resolve merge conflict

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      route: 'App',
+      route: 'landingPage',
       isSignedIn: false,
       user: {
         id: '',
